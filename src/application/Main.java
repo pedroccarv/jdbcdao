43e4d316133cf115ea672347116243d46cb3fe06
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-03 18:29:12 -0300

    add: class Department

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,14 @@
+package application;
+
+import model.entities.Department;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Department department = new Department(1, "Books");
+        System.out.println(department);
 
     }
 }
